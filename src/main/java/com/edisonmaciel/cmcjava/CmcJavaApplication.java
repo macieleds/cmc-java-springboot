463c67f26c91bbc5e2f883cf16ff5adf8c3a1a7a
Edison Maciel <maciel.eds@gmail.com>
2020-06-20 20:54:14 -0300

    Initial commit - Project created

---
+++ src/main/java/com/edisonmaciel/cmcjava/CmcJavaApplication.java
@@ -0,0 +1,15 @@
+package com.edisonmaciel.cmcjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmcJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmcJavaApplication.class, args);
+		
+		
+	}
+
+}
