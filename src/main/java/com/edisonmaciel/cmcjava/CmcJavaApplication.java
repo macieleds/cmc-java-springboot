cda4a27d69ad3622ebc13f749f643bf5b967ee3f
Edison Maciel <maciel.eds@gmail.com>
2020-06-22 16:01:01 -0300

    Creating instantiation operation

--- src/main/java/com/edisonmaciel/cmcjava/CmcJavaApplication.java
+++ src/main/java/com/edisonmaciel/cmcjava/CmcJavaApplication.java
@@ -1,14 +1,34 @@
 package com.edisonmaciel.cmcjava;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CmcJavaApplication {
+import com.edisonmaciel.cmcjava.domain.Categoria;
+import com.edisonmaciel.cmcjava.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CmcJavaApplication implements CommandLineRunner{
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CmcJavaApplication.class, args);
 		
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
 	}
 
