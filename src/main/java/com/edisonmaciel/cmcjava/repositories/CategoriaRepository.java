829fe927d3bae49d50f7d6f8f7f106c157eef734
unknown <maciel.eds@gmail.com>
2020-07-02 20:16:42 -0300

    Product and Many to Many Association

--- src/main/java/com/edisonmaciel/cmcjava/repositories/CategoriaRepository.java
+++ src/main/java/com/edisonmaciel/cmcjava/repositories/CategoriaRepository.java
@@ -1,6 +1,5 @@
 package com.edisonmaciel.cmcjava.repositories;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
