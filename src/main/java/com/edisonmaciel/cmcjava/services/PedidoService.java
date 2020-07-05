0f01ff09b051dba037bd7c549e71f4f9dd5064ad
unknown <maciel.eds@gmail.com>
2020-07-05 17:45:26 -0300

    Endpoint /pedidos/{id}

---
+++ src/main/java/com/edisonmaciel/cmcjava/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.edisonmaciel.cmcjava.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edisonmaciel.cmcjava.domain.Pedido;
+import com.edisonmaciel.cmcjava.repositories.PedidoRepository;
+import com.edisonmaciel.cmcjava.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repository;
+	
+	public Pedido buscar (Integer id) {
+		Optional<Pedido> obj = repository.findById(id);
+		return obj.orElseThrow (()-> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
