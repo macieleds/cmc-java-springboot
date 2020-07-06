4e4acb1efb72216bc6a72003cc89f1d2c665fb1b
unknown <maciel.eds@gmail.com>
2020-07-05 23:50:56 -0300

    Deleting Categoria - DELETE

--- src/main/java/com/edisonmaciel/cmcjava/repositories/EstadoRepository.java
+++ src/main/java/com/edisonmaciel/cmcjava/repositories/EstadoRepository.java
@@ -3,7 +3,6 @@ package com.edisonmaciel.cmcjava.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.edisonmaciel.cmcjava.domain.Categoria;
 import com.edisonmaciel.cmcjava.domain.Estado;
 
 @Repository
--- src/main/java/com/edisonmaciel/cmcjava/services/CategoriaService.java
+++ src/main/java/com/edisonmaciel/cmcjava/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.edisonmaciel.cmcjava.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.edisonmaciel.cmcjava.domain.Categoria;
 import com.edisonmaciel.cmcjava.repositories.CategoriaRepository;
+import com.edisonmaciel.cmcjava.services.exceptions.DataIntegrityException;
 import com.edisonmaciel.cmcjava.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,7 +30,17 @@ public class CategoriaService {
 	
 	public Categoria update (Categoria obj) {
 		find(obj.getId());
-		return repository.save(obj);
+		return repository.save(obj); 
+	}
+	
+	public void delete (Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
+		
 	}
 
 }
