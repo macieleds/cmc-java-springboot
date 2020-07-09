0ffc04f78fdac2676084bc48e7d769aeaeb1f182
unknown <maciel.eds@gmail.com>
2020-07-09 17:08:48 -0300

    Listing Categoria via GET with DTO

--- src/main/java/com/edisonmaciel/cmcjava/resources/CategoriaResource.java
+++ src/main/java/com/edisonmaciel/cmcjava/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.edisonmaciel.cmcjava.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.edisonmaciel.cmcjava.domain.Categoria;
+import com.edisonmaciel.cmcjava.dto.CategoriaDTO;
 import com.edisonmaciel.cmcjava.services.CategoriaService;
 
 @RestController
@@ -51,6 +54,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete (@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity <List<CategoriaDTO>> findAll () {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO (obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 		
