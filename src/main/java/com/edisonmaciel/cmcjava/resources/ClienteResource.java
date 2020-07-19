3f256eae0576ead7d77be79938454bfa9e725df9
unknown <maciel.eds@gmail.com>
2020-07-19 20:38:39 -0300

    Inserting a new Client with POST

--- src/main/java/com/edisonmaciel/cmcjava/resources/ClienteResource.java
+++ src/main/java/com/edisonmaciel/cmcjava/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.edisonmaciel.cmcjava.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,14 +12,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.edisonmaciel.cmcjava.domain.Categoria;
 import com.edisonmaciel.cmcjava.domain.Cliente;
 import com.edisonmaciel.cmcjava.dto.ClienteDTO;
+import com.edisonmaciel.cmcjava.dto.ClienteNewDTO;
 import com.edisonmaciel.cmcjava.services.ClienteService;
 
 @RestController
@@ -33,6 +38,15 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find (@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert (@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 		
 	@PutMapping(value="/{id}")
--- src/main/java/com/edisonmaciel/cmcjava/services/ClienteService.java
+++ src/main/java/com/edisonmaciel/cmcjava/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.edisonmaciel.cmcjava.domain.Cidade;
 import com.edisonmaciel.cmcjava.domain.Cliente;
+import com.edisonmaciel.cmcjava.domain.Endereco;
+import com.edisonmaciel.cmcjava.domain.enums.TipoCliente;
 import com.edisonmaciel.cmcjava.dto.ClienteDTO;
+import com.edisonmaciel.cmcjava.dto.ClienteNewDTO;
 import com.edisonmaciel.cmcjava.repositories.ClienteRepository;
+import com.edisonmaciel.cmcjava.repositories.EnderecoRepository;
 import com.edisonmaciel.cmcjava.services.exceptions.DataIntegrityException;
 import com.edisonmaciel.cmcjava.services.exceptions.ObjectNotFoundException;
 
@@ -22,15 +28,23 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repository;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find (Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
 		return obj.orElseThrow (()-> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
+	
+	
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
-		return repository.save(obj);
+		repository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
@@ -60,6 +74,21 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	} 
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	} 
 	
 	private void updateData(Cliente newObj, Cliente obj) {
